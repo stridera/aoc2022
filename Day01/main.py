3d0c18467504a3d8aec79e19994dc645441e3a48
Matthew Jones <stridera@gmail.com>
2022-12-16 17:34:31 -0800

    Append(day1)

---
+++ Day01/main.py
@@ -0,0 +1,13 @@
+# elf = []
+# sum = 0
+# for line in open('input'):
+#     line = line.strip()
+#     if line == '':
+#         elf.append(sum)
+#         sum = 0
+#     else:
+#         sum += int(line)
+
+# sorted_elves = sorted({sum: i for i, sum in enumerate(sorted(elf))}
+# print(sorted_elves)
+print(69108 + 69171 + 69177)
